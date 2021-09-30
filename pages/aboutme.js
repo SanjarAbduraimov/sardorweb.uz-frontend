bfa16beebd6b9daaae1766d3961d26b2443e7b6f
Ángel Vazurio <angelCL0@outlook.com>
2021-09-29 20:55:00 -0400

    fixed

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -1,11 +1,12 @@
 import Cta from '../components/Cta/Cta';
 import Layout from '../components/Layout';
-import { Hero } from '../screens/AboutMe';
+import { Companies, Hero } from '../screens/AboutMe';
 
 export default function AboutMe() {
   return (
     <Layout>
       <Hero />
+      <Companies />
       <Cta />
     </Layout>
   );
---
+++ screens/AboutMe/Companies/Companie/Companie.js
@@ -0,0 +1,65 @@
+import { Frame, Text, Wrapper } from '../../../../components/UIKit';
+import Image from 'next/image';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 44px auto;
+  justify-items: flex-start;
+  align-items: flex-start;
+  grid-gap: 24px;
+  padding: 20px;
+  border-radius: 8px;
+  transition: all 0.2s cubic-bezier(0.455, 0.03, 0.515, 0.955) linear;
+
+  svg {
+    fill: white;
+  }
+
+  ${Frame} {
+    background: #222233;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  &:hover {
+    background-color: #222233;
+
+    ${Frame} {
+      background-color: green;
+    }
+
+    svg {
+      fill: #222233;
+    }
+  }
+`;
+
+export default function Companie(props) {
+  return (
+    <Container>
+      <Frame
+        position='relative'
+        overflow='hidden'
+        height={[44, 44]}
+        width={[44, 44, 44]}
+        borderRadius={['50%']}
+      >
+        <svg
+          width='15'
+          height='15'
+          viewBox='0 0 15 15'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path fill-rule='evenodd' clip-rule='evenodd' d={props.icon} />
+        </svg>
+      </Frame>
+      <Wrapper display={['grid']} gridGap={[2]}>
+        <Text size='bodyBold'>{props.name}</Text>
+        <Text size='caption2'>{props.position}</Text>
+      </Wrapper>
+    </Container>
+  );
+}
