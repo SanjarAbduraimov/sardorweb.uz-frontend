581ed23c7681805bfb9f87ca6cc9d4310e0cfb88
Ángel Vazurio <angelCL0@outlook.com>
2021-09-29 22:54:45 -0400

    fixed

--- components/BlogCard/BlogCard.js
+++ components/BlogCard/BlogCard.js
@@ -1,7 +1,13 @@
 import { Wrapper, Text, Frame } from '../UIKit';
 import Image from 'next/image';
 import styled from 'styled-components';
-import Link from 'next/link';
+
+const Button = styled.a`
+  color: #006ada;
+  font-size: 14px;
+  font-weight: bold;
+  margin-right: 6px;
+`;
 
 const Container = styled.div`
   position: relative;
@@ -10,7 +16,7 @@ const Container = styled.div`
   align-items: flex-start;
 
   svg {
-    fill: white;
+    fill: #006ada;
   }
 
   &:hover {
@@ -18,17 +24,14 @@ const Container = styled.div`
       background-color: #29c83f;
     }
 
+    ${Button} {
+      color: #29c83f;
+    }
+
     svg {
-      fill: #222233;
+      fill: #29c83f;
     }
   }
-`;
-
-const Button = styled.a`
-  color: #006ada;
-  font-size: 14px;
-  font-weight: bold;
-  margin-right: 6px;
 `;
 
 export default function BlogCard(props) {
@@ -51,35 +54,35 @@ export default function BlogCard(props) {
           onLoadingComplete=''
         />
       </Frame>
+      <Frame>
+        <Wrapper p={[40]} display={['grid']} gridGap={[24]}>
+          <Wrapper display={['grid']} gridGap={[16]}>
+            <Text size='heading4'>{props.title}</Text>
+            <Text size='caption'>
+              You must engrave deeply in your mind and never forget: your
+              emotional commitment to what you are doing will be translated into
+              your work.
+            </Text>
+          </Wrapper>
 
-      <Wrapper p={[40]} display={['grid']} gridGap={[24]}>
-        <Wrapper display={['grid']} gridGap={[16]}>
-          <Text size='heading4'>{props.title}</Text>
-          <Text size='caption'>
-            You must engrave deeply in your mind and never forget: your
-            emotional commitment to what you are doing will be translated into
-            your work.
-          </Text>
+          <Wrapper display={['flex']} alignItems={['center']}>
+            <Button>Read Full Post</Button>
+            <svg
+              width='15'
+              height='15'
+              viewBox='0 0 15 15'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                d='M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z'
+                fillRule='evenodd'
+                clipRule='evenodd'
+              ></path>
+            </svg>
+          </Wrapper>
         </Wrapper>
-
-        <Wrapper display={['flex']} alignItems={['center']}>
-          <Button>Read Full Post</Button>
-          <svg
-            width='15'
-            height='15'
-            viewBox='0 0 15 15'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path
-              d='M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z'
-              fill='#006ada'
-              fillRule='evenodd'
-              clipRule='evenodd'
-            ></path>
-          </svg>
-        </Wrapper>
-      </Wrapper>
+      </Frame>
     </Container>
   );
 }
