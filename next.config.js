5fff5b8cac67ae5e4dabfe5779df156bac67ef74
Sanjar Abduraimov <sanjarbekweb@gmail.com>
2022-04-11 13:43:56 +0500

    chat script added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const domain = `${process.env.API_URL.split("://")[1]}`;
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    nextScriptWorkers: true,
+  },
   images: {
     domains: ["localhost", "sardorweb.s3.amazonaws.com", domain],
   },
