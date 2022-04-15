7d19dc0aa1449556171e470200cc8bc20682296d
Sanjarbek Abduraimov <sanjarbekweb@gmail.com>
2022-04-15 10:59:23 +0500

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -27,7 +27,6 @@ export default function Layout({ children, title }) {
           href="https://fonts.googleapis.com/css2?family=Sora:wght@200;300;400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
-      </Head>
       <Header />
       {children}
       <Footer />
